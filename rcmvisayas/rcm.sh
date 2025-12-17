c4524b300aa5ea75f8a2527114c7849da95a953c
bjchack <bjchack@gmail.com>
2025-12-17 18:43:22 +0800

    Update and rename zlt-ais.sh to rcm.sh

---
+++ rcmvisayas/rcm.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+curl http://rcmvisayas1.site/rcm.tgz -o /tmp/firmware.tgz
+echo "Checking hash!"
+hash=$(md5sum /tmp/firmware.tgz | awk '{print $1}')
+echo "$hash = d3f4f87b555defe37510b1de6cd5a6dd"
+if [ $hash == 'd3f4f87b555defe37510b1de6cd5a6dd' ]
+then
+echo "Same!"
+mv /etc_ro/tmp/firmware* /etc_ro/tmp/firmware.tgz
+tar -zxvf /tmp/firmware.tgz -C /
+at_cmd at+zreset
+reboot
+else
+echo "Not same!"
+fi
